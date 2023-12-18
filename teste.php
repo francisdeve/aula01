739e1057dfe434c4eba589740bf1955b8b64c9a0
Francis Moreira <francis@authenticacertificadora.com.br>
2023-12-18 15:25:16 -0300

    if condicional idade

---
+++ teste.php
@@ -0,0 +1,12 @@
+<h1>Meu primeiro sistema</h1>
+<?php
+echo "meu primeiro sistema";
+echo "<h1>meu primeiro sistema</h1>";
+$nome="Francis Moreira";
+$idade=19;
+if($idade=>18){ 
+    echo 'é maior de idade';
+}else{ 
+     echo 'é menor de idade';
+}
+?>
